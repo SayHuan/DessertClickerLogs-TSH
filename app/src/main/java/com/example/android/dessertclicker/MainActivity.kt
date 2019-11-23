ac2e73f65f8206742c6cefd75935d518a139e82b
Tan Say Huan <tansh-jm17@student.tarc.edu.my>
2019-11-23 11:24:09 +0800

    updated

--- app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -58,7 +58,7 @@ class MainActivity : AppCompatActivity() {
     private var dessertsSold = 0
     private lateinit var dessertTimer: DessertTimer;
 
-    //...
+    //aaa
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
 
